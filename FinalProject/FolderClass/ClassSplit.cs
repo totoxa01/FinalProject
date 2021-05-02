9535739e0e05593b51585c5519c828b32809a806
glowwormsv <adm_kot@mail.ru>
2021-05-02 18:16:30 +0300

    Add project files.

---
+++ FinalProject/FolderClass/ClassSplit.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalProject.FolderClass
+{
+    class ClassSplit
+    {
+        public static string[] SpliString(string s)
+        {
+            string[] words = s.Split(new char[] { '\\' }, StringSplitOptions.RemoveEmptyEntries);
+            return words;
+        }
+    }
+}
---
+++ FinalProject/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace FinalProject
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string dirName = "E:\\";
+            //опеределение каталогов по указанному пути
+            try
+            {
+                if (Directory.Exists(dirName))
+                {
+                    Console.WriteLine("Подкаталоги:");
+                    string[] dirs = Directory.GetDirectories(dirName);
+                    int i = 0;
+                    foreach (string s in dirs)
+                    {
+                        Console.WriteLine(s);//диск с каталогами
+                        //string[] dirsP = Directory.GetDirectories(s);
+                        //if (Directory.Exists(dirs[i]))
+                        //{
+                        //    foreach (string s1 in dirsP)
+                        //    {
+                        //        string[] text=FolderClass.ClassSplit.SpliString(s1);
+                        //        foreach(string j in text)
+                        //        {
+                        //            Console.Write(" ");
+                        //            Console.WriteLine(j);
+                        //            Console.Write(" ");
+                        //        }                                
+                        //    }
+                        //}
+                        //i++;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            
+                
+            Console.ReadKey();
+        }
+    
+    }
+}
